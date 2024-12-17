510a9a0d4207571124513c7be19a6025a99d184b
Edward Xu <12210613@mail.sustech.edu.cn>
2024-12-17 15:03:22 +0800

    超绝50s版本
    选择singleInputJob为主类打包

--- src/main/java/mapper/OrderMapper.java
+++ src/main/java/mapper/OrderMapper.java
@@ -4,19 +4,32 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+
 public class OrderMapper extends Mapper<LongWritable, Text, Text, Text> {
-    private String securityId;
+    private String securityId; // 邮票代码
+    private final String[] fields = new String[20]; // 预分配数组
+
+    protected void setup(Context context) {
+        securityId = context.getConfiguration().get("securityID");
+    }
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] fields = value.toString().split("\t");
-        securityId = context.getConfiguration().get("securityID");
-        if (fields.length == 20) { // 检查字段数量是否满足要求
-            String sID = fields[8];
-            if (sID.equals(securityId)){
-                String applSeqNum = fields[7];
-                String transactTime = fields[12];
-                context.write(new Text(applSeqNum), new Text(transactTime));
+        String line = value.toString();
+        int start = 0, fieldIndex = 0;
+
+        for (int i = 0; i < line.length() && fieldIndex < 20; i++) {
+            if (line.charAt(i) == '\t') {
+                fields[fieldIndex++] = line.substring(start, i);
+                start = i + 1;
             }
+        }
+        if (fieldIndex < 20) {
+            fields[fieldIndex] = line.substring(start);
+        }
+
+        if (fields.length == 20 && fields[8].equals(securityId)) {
+            context.write(new Text(fields[7]), new Text(fields[12]));
         }
     }
 }
--- src/main/java/utils/TimeWindowUtils.java
+++ src/main/java/utils/TimeWindowUtils.java
@@ -7,35 +7,35 @@ import java.util.List;
 
 public class TimeWindowUtils {
     public static List<String> generateTimeWindows(String intervalStr) throws Exception{
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-        List<String> timeWindows = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS"); //定义时间格式
+        List<String> timeWindows = new ArrayList<>(); //初始化时间窗口列表
         int interval = Integer.parseInt(intervalStr) * 1000; //转换为毫秒
 
-        String morningStart2 = "20190102093000000";
-        String morningEnd2 = "20190102113000000";
+        String morningStart2 = "20190102093000000"; //定义上午开始时间
+        String morningEnd2 = "20190102113000000"; //定义上午结束时间
 
-        generateTimeWindowsForPeriod(sdf, morningStart2, morningEnd2, interval, timeWindows);
+        generateTimeWindowsForPeriod(sdf, morningStart2, morningEnd2, interval, timeWindows); //生成上午时间窗口
 
-        String afternoonStart1 = "20190102130000000";
-        String afternoonEnd1 = "20190102150000000";
+        String afternoonStart1 = "20190102130000000"; //定义下午开始时间
+        String afternoonEnd1 = "20190102150000000"; //定义下午结束时间
 
-        generateTimeWindowsForPeriod(sdf,afternoonStart1,afternoonEnd1,interval,timeWindows);
+        generateTimeWindowsForPeriod(sdf,afternoonStart1,afternoonEnd1,interval,timeWindows); //生成下午时间窗口
 
-        return timeWindows;
+        return timeWindows; //返回时间窗口列表
     }
 
     private static void generateTimeWindowsForPeriod(SimpleDateFormat sdf, String start, String end, int interval, List<String> timeWindows) throws Exception{
-        Date startTime = sdf.parse(start);
-        Date endTime = sdf.parse(end);
+        Date startTime = sdf.parse(start); //将字符串转换为时间
+        Date endTime = sdf.parse(end); //将字符串转换为时间
 
         while(startTime.before(endTime)){ //严格小于
-            Date nextTime = new Date(startTime.getTime() + interval);
-            if(nextTime.after(endTime)){
-                timeWindows.add(sdf.format(startTime)+"-"+sdf.format(endTime));
+            Date nextTime = new Date(startTime.getTime() + interval); //计算下一个时间
+            if(nextTime.after(endTime)){ //严格大于
+                timeWindows.add(sdf.format(startTime)+"-"+sdf.format(endTime)); //将时间窗口添加到列表
                 break;
             }
-            timeWindows.add(sdf.format(startTime)+"-"+sdf.format(nextTime));
-            startTime = nextTime;
+            timeWindows.add(sdf.format(startTime)+"-"+sdf.format(nextTime)); //将时间窗口添加到列表
+            startTime = nextTime; //更新开始时间
         }
     }
 
